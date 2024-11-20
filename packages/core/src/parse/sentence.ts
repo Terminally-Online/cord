42f1a53af57227fb67b0d8f2850a4e1294b5e83d
nftchance <chance@utc24.io>
2024-11-20 11:09:09 -0600

    feat: prepare architecture for extended parsing

--- packages/core/src/parse/sentence.ts
+++ packages/core/src/parse/sentence.ts
@@ -1,62 +1,11 @@
-import { isEvmType, validateEvmValue, validateInputSequence } from "./validate";
 import {
-    PLACEHOLDER_PATTERN,
-    InputReference,
     ParsedCordSentence,
     Result,
-    EvmType,
-    CompoundType,
-    ConstantType,
-    InputType,
-} from "./lib";
-
-const parseTypeString = (
-    typeString: string
-): {
-    type: InputType;
-    defaultValue?: string;
-} => {
-    const parts = typeString.split(":");
-    const types: (EvmType | ConstantType)[] = [];
-    const defaults: (string | undefined)[] = [];
-
-    parts.forEach((part) => {
-        const [typeStr, defaultValue] = part.split("=");
-
-        const type: EvmType | ConstantType = isEvmType(typeStr)
-            ? (typeStr as EvmType)
-            : { constant: typeStr };
-
-        if (defaultValue !== undefined) {
-            if (!validateEvmValue(defaultValue, type)) {
-                throw new Error(
-                    typeof type === "object"
-                        ? `Invalid default value "${defaultValue}" for constant(${type.constant})`
-                        : `Invalid default value "${defaultValue}" for type ${type}`
-                );
-            }
-        }
-
-        types.push(type);
-        defaults.push(defaultValue);
-    });
-
-    const type: InputType =
-        types.length > 1
-            ? {
-                  baseType: types[0],
-                  metadata: types.slice(1),
-              }
-            : types[0];
-
-    const definedDefaults = defaults.filter(
-        (d): d is string => d !== undefined
-    );
-    const defaultValue =
-        definedDefaults.length > 0 ? definedDefaults.join(":") : undefined;
-
-    return { type, defaultValue };
-};
+    InputReference,
+    PLACEHOLDER_PATTERN,
+} from "../lib";
+import { parseTypeString } from "./types";
+import { validateInputSequence } from "../validate";
 
 export const parseCordSentence = (
     sentence: string
@@ -125,17 +74,4 @@ export const parseCordSentence = (
                     : "Unknown parsing error",
         };
     }
-};
-
-export const parseCompoundType = (type: string): CompoundType | null => {
-    const types = type.split(":");
-    if (types.length === 1) return null;
-
-    const [baseType, ...metadata] = types;
-    if (!isEvmType(baseType) || !metadata.every(isEvmType)) return null;
-
-    return {
-        baseType: baseType as EvmType,
-        metadata: metadata as EvmType[],
-    };
 };
--- packages/core/test/index.ts
+++
@@ -1,5 +0,0 @@
-export const SENTENCES = [
-    "Deposit {0} {1} to {2}.",
-    "Transfer {0} {1|:} to {2}.",
-    "Transfer {0<amount:[if:1|1=721:1:else:uint256]=1>} {1<token:address>|:} {2<id:uint256>} to {3<recipient:address>}.",
-];
