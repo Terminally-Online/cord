a3aef5ab162c90296299e64c6d7c7702cc6db1b1
nftchance <chance@utc24.io>
2024-11-29 15:20:48 -0600

    fix: remove all react references

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,7 +1,6 @@
 export * from "./input";
 export * from "./lib";
 export * from "./parse";
-export * from "./react/useCord";
 export * from "./state";
 export * from "./validate";
 export * from "./values";
